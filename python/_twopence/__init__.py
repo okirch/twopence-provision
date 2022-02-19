c643a304f0059d78b7d552dbd3fe5fcde0184b43
Olaf Kirch <okir@suse.de>
2022-02-19 11:03:22 +0100

    Add public helper function queryPlatformFeatures
    Signed-off-by: Olaf Kirch <okir@suse.de>

--- python/_twopence/__init__.py
+++ python/_twopence/__init__.py
@@ -11,3 +11,21 @@ from .manifest import BOM
 from .backend import Backend
 from .topology import TestTopology
 from .config import Config, ConfigError, RequirementsManager
+
+def queryPlatformFeatures(platformName):
+	import twopence
+
+	config = Config("/no/where")
+
+	for path in twopence.global_config_files:
+		config.load(path)
+
+	# Note: we load global config files first; THEN
+	# we add user directories to the config search path.
+	config.addDirectory(twopence.user_config_dir)
+
+	platform = config.getPlatform(platformName)
+	if platform is None:
+		return None
+
+	return set(platform.features)
